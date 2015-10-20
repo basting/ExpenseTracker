473daa7ff019914ddad348358ce2760626b6b14e
basting <bastin.gomez@gmail.com>
2015-10-19 21:39:04 -0400

    changes

--- app/src/main/java/com/bighi/expensetracker/MainActivity.java
+++ app/src/main/java/com/bighi/expensetracker/MainActivity.java
@@ -5,11 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtDescription;
+    private EditText txtAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        txtDescription = (EditText) findViewById(R.id.txtDescription);
+        txtAmount = (EditText) findViewById(R.id.txtAmount);
+        txtAmount.addTextChangedListener(new CurrencyTextWatcher());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +62,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void btnAddClick(View view) {
+        Toast.makeText(getApplicationContext(), txtDescription.getText() + ":" + txtAmount.getText(), Toast.LENGTH_LONG).show();
+    }
+
+    class CurrencyTextWatcher implements TextWatcher {
+        String current="";
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(!s.toString().equals(current)){
+                txtAmount.removeTextChangedListener(this);
+
+                String cleanString = s.toString().replaceAll("[$,.]", "");
+
+                double parsed = Double.parseDouble(cleanString);
+                String formatted = NumberFormat.getCurrencyInstance().format((parsed/100));
+
+                current = formatted;
+                txtAmount.setText(formatted);
+                txtAmount.setSelection(formatted.length());
+
+                txtAmount.addTextChangedListener(this);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
     }
 }
