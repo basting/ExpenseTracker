e489e85c51ed8c02c0daa6b0699e4cf66006771c
basting <bastin.gomez@gmail.com>
2015-10-21 19:59:04 -0400

    Settings screen changes

--- app/src/main/java/com/bighi/expensetracker/MainActivity.java
+++ app/src/main/java/com/bighi/expensetracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bighi.expensetracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -72,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, SettingsActivity.class);
+            startActivityForResult(intent, 0);
             return true;
         }
 
