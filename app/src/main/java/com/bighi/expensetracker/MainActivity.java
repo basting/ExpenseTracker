efe33ae17b155f4a248495a285e38338c8920d3d
basting <bastin.gomez@gmail.com>
2015-10-22 20:07:17 -0400

    currency desc change

--- app/src/main/java/com/bighi/expensetracker/MainActivity.java
+++ app/src/main/java/com/bighi/expensetracker/MainActivity.java
@@ -86,7 +86,9 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onSharedPreferenceChanged(SharedPreferences sp, String s) {
-                    txtCurrency.setText(sp.getString("currency_pref", "CAD"));
+                    String currShort = sp.getString("currency_pref", "CAD");
+                    String currLong = sp.getAll().get(currShort).toString();
+                    txtCurrency.setText(currLong);
                 }
             };
         }
