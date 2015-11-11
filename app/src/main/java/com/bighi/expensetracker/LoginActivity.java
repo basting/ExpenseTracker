3eef477ceddcd0e0583ea71bf44d09d2d6714368
basting <bastin.gomez@gmail.com>
2015-11-10 20:51:40 -0500

    Login screen

--- app/src/main/java/com/bighi/expensetracker/LoginActivity.java
+++ app/src/main/java/com/bighi/expensetracker/LoginActivity.java
@@ -3,6 +3,7 @@ package com.bighi.expensetracker;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -292,7 +293,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     @Override
     public void finish() {
-
+        Intent homeIntent = new Intent(getApplicationContext(),ExpensesListActivity.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
     }
 
     /**
