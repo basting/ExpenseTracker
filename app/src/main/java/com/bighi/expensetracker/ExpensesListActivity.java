66ab84607306d001f6b21958141a4a988f1e47d8
basting <bastin.gomez@gmail.com>
2015-11-16 15:56:20 -0500

    Action bar

--- app/src/main/java/com/bighi/expensetracker/ExpensesListActivity.java
+++ app/src/main/java/com/bighi/expensetracker/ExpensesListActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -111,4 +112,18 @@ public class ExpensesListActivity extends AppCompatActivity {
         mExpenseListAdapter.cleanup();
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
